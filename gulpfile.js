fa763af2dafc696c543018dcc856a2fd6a64da47
Your Name <you@example.com>
2022-05-31 20:54:12 +1000

    final_1

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,20 @@ const sass = require('gulp-sass')(require('sass'));
 const rename = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 // Static server
 gulp.task('server', function () {
-    browserSync.init({
+
+    browserSync({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function () {
@@ -23,13 +29,18 @@ gulp.task('styles', function () {
         }))
         .pipe(autoprefixer())
         .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 })
 
 gulp.task('watch', function () {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"))
-    gulp.watch("src/*.html").on("change", browserSync.reload);
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel("styles"))
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+});
+gulp.task('html', function () {
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"));
 });
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
